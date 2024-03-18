4c84c310655598e1e4e2839f13d009fbabc7f953
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-03-17 23:35:36 -0600

    the layout proyect and add 2 buttons sobre mi & proyectos

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Header from "./components/Header"
+function App() {
+
+  return (
+    <>
+      <section className="container">
+        <header>
+          <Header/>
+        </header>
+        <aside>Aside</aside>
+        <main>Content</main>
+        <footer>Foter</footer>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div className="button">
+        <button>Sobre mi</button>
+        <button>Proyectos</button>
+    </div>
+  )
+}
+
+export default Header
