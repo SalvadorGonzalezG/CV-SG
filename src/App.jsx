b8ee390aa13f0826bf93c26c2a3bda2c236a2d87
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-03-19 02:28:34 -0600

    add carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-
 import Header from "./components/Header"
+import Aside from "./components/Aside"
 function App() {
 
   return (
     <>
       <section className="container">
         <header>
-          <Header/>
+          <Header />
         </header>
-        <aside>Aside</aside>
+        <aside>
+          <Aside />
+        </aside>
         <main>Content</main>
         <footer>Foter</footer>
       </section>
