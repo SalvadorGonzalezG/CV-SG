5c8556ea909442b29562ef6210726f3b3262595c
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-03-21 15:09:02 -0600

    data added

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -3,6 +3,8 @@ import { MdPhone, MdOutlinePhoneIphone } from "react-icons/md";
 import { IoIosMail } from "react-icons/io";
 import { FcContacts } from "react-icons/fc";
 import { TiHome } from "react-icons/ti";
+import { RiMapPinUserFill, RiPinDistanceFill } from "react-icons/ri";
+
 
 // importamos los estilos directamente del documento donde se colocaron
 import '../index.css';
@@ -13,7 +15,10 @@ const Popup = ({ onClose }) => {
       <div className="popup-content" onClick={(e) => e.stopPropagation()}>
         <button className="close-button" onClick={onClose}>Cerrar</button>
         <img className='img' src="https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg" alt="Yo" />
+    
         <h2><FcContacts/> Datos de contacto.</h2>
+        <h4><RiMapPinUserFill/> Direccion:</h4>
+        <p> Ciudad Nezahualcoyotl Edo.Méx, Col.Benito Juarez. CP.57000</p>
         <h4><IoIosMail/> Correo(s):</h4>
         <p>salvadorGonzalezG@outlook.com <br /> salvadorsopas@gmail.com</p>
         <h4><MdPhone/> Telefono(s):</h4>
