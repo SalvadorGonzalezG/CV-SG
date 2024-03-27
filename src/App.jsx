3a11f5f1a1e238cfcac7cff0eaaf3f0c6ff64776
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-03-26 20:03:30 -0600

    agregando contenido & exportandolo en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Aside from "./components/Aside"
 import Carrousel from "./components/Carrousel1"
 import Carrou from "./components/Carrou" //Segundo Carrousel incluido en el aside
 import Cslhab from "./components/Cslhab"
+import Content from "./components/Content"
 
 function App() {
 
@@ -17,7 +18,9 @@ function App() {
           <Carrou/>
           <Cslhab/>
         </aside>
-        <main>Content</main>
+        <main>
+          <Content/>
+        </main>
         <footer>Foter</footer>
       </section>
     </>
