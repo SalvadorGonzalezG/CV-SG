07af4896418fdcb910cff5a478454efa34576131
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-04-10 17:13:24 -0600

    Se a implementado una sección de proyecto en los cuales se a creado un boton y diversos screnShots de dichos proyectos tomados desde la api de netlify

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,22 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 const Projects = () => {
+  // Inicializamos el estado de los proyectos con un array vacion.
+  const[projects, setProjects] = useState([])
 
+  useEffect(()=>{
+// token de acceso.
+ const token = 'nfp_sdnirUEZAPcPsh8KkmDa9LvtSRXKCUu4a6e8'
+
+    axios.get("https://api.netlify.com/api/v1/sites",{
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(response => {
+      console.log('res api', response.data)
+      setProjects(response.data);
+    })
+    .catch((error) => {console.error("Error al obtener la lista de proyectos", error)})
+  },[])
   return (
-    <>
-        <h1 className="otro">Proyectos.</h1>
-        <div className="proyectos">
-          <div>one</div>
-          <div>two</div>
-          <div>two</div>
-          <div>two</div>
-          <div>two</div>
-          <div>two</div>
-          <div>two</div>
-          <div>two</div>
-        </div>
-    </>
-    
-  )
-}
+    <div className="projects-container">
+      <h1 className="otro">Proyectos.</h1>
+      <div className="proyectos">
+        {projects.map((project)=>(
+          <div key={project.id}>
+            <img src={project.screenshot_url} alt={project.name} />
+            
+            <button className="btn">
+              <a href={project.url} target="_black" rel="noopener noreferrer">Ver proyecto</a> 
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
