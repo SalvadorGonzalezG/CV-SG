d432652c09337e2ea486b9b395f1b844539d24fe
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-04-22 17:29:48 -0600

    Modificando el contenido y agregando datos HTML

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,26 +2,41 @@
 const Content = () => {
   return (
     <>
-    <div className="txt-cont">
-    <h4>EXPERIENCIA LABORAL.</h4>
-        <p>EMPRESA: CINEPOLIS. <br />
-Tiempo Laborado: 1 año 1 mes (17/05/15 – 27/07/16).
-Puesto a desempeñar: empleado general, 8 meses en taquilla, 4 meses 
-en el área de dulcería bebidas y alimentos, punto de venta, atención y 
-servicio al cliente, manejo de efectivo.</p>
-    </div>
-    <div className="conte2">
-        <h4>Casino Life</h4>
-        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-    </div>
-    <div className="conte3">
+      <div className="txt-cont">
+        <h2>EXPERIENCIA LABORAL.</h2>
+        <h4> EMPRESA: CINEPOLIS. </h4>
+        <p>
+          Tiempo Laborado: 1 año 1 mes (17/05/15 – 27/07/16). <br />
+          Puesto a desempeñar: empleado general, 8 meses en taquilla, 4 meses
+          en el área de dulcería bebidas y alimentos, punto de venta, atención y
+          servicio al cliente, manejo de efectivo.</p>
+
+        <h4>EMPRESA: CIRSA, CASINO LIFE</h4>
+        <p>
+          Tiempo Laborado: 1 año 3 meses (15/09/16 – 30/12/17). <br />
+          Puesto a desempeñar: 3 meses en academia para dealers (croupiers) en
+          casino Life Emotion, posteriormente 1 año en casino Life Delvalle en el
+          área de mesas como dealer (croupier).
+        </p>
+        <h4>EMPRESA: CIRSA MÉXICO.</h4>
+        <p>
+          Tiempo Laborado: (). <br />
+          Puesto a desempeñar: Coordinador de inspección y riesgos encargado 
+de desarrollar informes relacionados con las malas prácticas que lleven 
+a infringir las normas y políticas establecidas para mantener la 
+confidencialidad de la información, además de detectar y reportar 
+incidencias ocurridas en 11 salas de los 28 casinos que se encuentran 
+en la CDMX
+        </p>
+      </div>
+      <div className="conte3">
         <h4>Cirsa México.</h4>
         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-    </div>
-    <div className="conte4">
+      </div>
+      <div className="conte4">
         <h4>Desarrollador de sofware; experience</h4>
         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-    </div>
+      </div>
     </>
   )
 }
