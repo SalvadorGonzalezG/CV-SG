23d760e3b9b508c30099945e8915474432aa2b2e
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-05-01 23:17:15 -0600

    added content in academy formation

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -21,17 +21,27 @@ const Content = () => {
         <h4>EMPRESA: CIRSA MÉXICO.</h4>
         <p>
           Tiempo Laborado: (). <br />
-          Puesto a desempeñar: Coordinador de inspección y riesgos encargado 
-de desarrollar informes relacionados con las malas prácticas que lleven 
-a infringir las normas y políticas establecidas para mantener la 
-confidencialidad de la información, además de detectar y reportar 
-incidencias ocurridas en 11 salas de los 28 casinos que se encuentran 
-en la CDMX
+          Puesto a desempeñar: Coordinador de inspección y riesgos encargado
+          de desarrollar informes relacionados con las malas prácticas que lleven
+          a infringir las normas y políticas establecidas para mantener la
+          confidencialidad de la información, además de detectar y reportar
+          incidencias ocurridas en 11 salas de los 28 casinos que se encuentran
+          en la CDMX
         </p>
       </div>
-      <div className="conte3">
-        <h4>Cirsa México.</h4>
-        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+      <div className="txt-cont">
+        <h2>FORMACIÓN ACADEMICA.</h2>
+        <h4>ESCUELA: AUM IZTAPALAPA</h4>
+        <p>
+          2015-2019 UNIVERSIDAD AUTÓNOMA METORPOLITANA, UNIDAD IZTAPALAPA.
+          Licenciatura en matemáticas (TRUNCA). </p>
+
+        <h4>ESCUELA: IPN UPIICSA.</h4>
+        <p>2022 - 2024 INSTITUTO POLITECNICO NACIONAL, UPIICSA.
+          Licenciatura en Ciencias de la informática.</p>
+        <h4>ESCUELA: DEV-F.</h4>
+        <p>2023 - 2024 BOOTCAMP DEV-F.
+          Master en coding.</p>
       </div>
       <div className="conte4">
         <h4>Desarrollador de sofware; experience</h4>
